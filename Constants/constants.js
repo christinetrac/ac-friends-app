7c9abfd7dfcf7e0076c8bc4af0c247cb823b5571
christinetrac <christineetrac@gmail.com>
2020-11-28 00:21:07 -0500

    Added png files of villager species and gender.

--- Constants/constants.js
+++ Constants/constants.js
@@ -41,4 +41,147 @@ const SPECIES = {
     wolf: 'Wolf'
 };
 
-export { GENDER, SPECIES };
+const SPECIES_IMG = [
+    {
+        species: SPECIES.alligator,
+        file: require('../Images/Species/Alligator.png')
+    },
+    {
+        species: SPECIES.anteater,
+        file: require('../Images/Species/Anteater.png')
+    },
+    {
+        species: SPECIES.bear,
+        file: require('../Images/Species/Bear.png')
+    },
+    {
+        species: SPECIES.bird,
+        file: require('../Images/Species/Bird.png')
+    },
+    {
+        species: SPECIES.bull,
+        file: require('../Images/Species/Bull.png')
+    },
+    {
+        species: SPECIES.cat,
+        file: require('../Images/Species/Cat.png')
+    },
+    {
+        species: SPECIES.chicken,
+        file: require('../Images/Species/Chicken.png')
+    },
+    {
+        species: SPECIES.cow,
+        file: require('../Images/Species/Cow.png')
+    },
+    {
+        species: SPECIES.cub,
+        file: require('../Images/Species/Cub.png')
+    },
+    {
+        species: SPECIES.deer,
+        file: require('../Images/Species/Deer.png')
+    },
+    {
+        species: SPECIES.dog,
+        file: require('../Images/Species/Dog.png')
+    },
+    {
+        species: SPECIES.duck,
+        file: require('../Images/Species/Duck.png')
+    },
+    {
+        species: SPECIES.eagle,
+        file: require('../Images/Species/Eagle.png')
+    },
+    {
+        species: SPECIES.elephant,
+        file: require('../Images/Species/Elephant.png')
+    },
+    {
+        species: SPECIES.frog,
+        file: require('../Images/Species/Frog.png')
+    },
+    {
+        species: SPECIES.goat,
+        file: require('../Images/Species/Goat.png')
+    },
+    {
+        species: SPECIES.gorilla,
+        file: require('../Images/Species/Gorilla.png')
+    },
+    {
+        species: SPECIES.hamster,
+        file: require('../Images/Species/Hamster.png')
+    },
+    {
+        species: SPECIES.hippo,
+        file: require('../Images/Species/Hippo.png')
+    },
+    {
+        species: SPECIES.horse,
+        file: require('../Images/Species/Horse.png')
+    },
+    {
+        species: SPECIES.kangaroo,
+        file: require('../Images/Species/Kangaroo.png')
+    },
+    {
+        species: SPECIES.koala,
+        file: require('../Images/Species/Koala.png')
+    },
+    {
+        species: SPECIES.lion,
+        file: require('../Images/Species/Lion.png')
+    },
+    {
+        species: SPECIES.monkey,
+        file: require('../Images/Species/Monkey.png')
+    },
+    {
+        species: SPECIES.mouse,
+        file: require('../Images/Species/Mouse.png')
+    },
+    {
+        species: SPECIES.octopus,
+        file: require('../Images/Species/Octopus.png')
+    },
+    {
+        species: SPECIES.ostrich,
+        file: require('../Images/Species/Ostrich.png')
+    },
+    {
+        species: SPECIES.penguin,
+        file: require('../Images/Species/Penguin.png')
+    },
+    {
+        species: SPECIES.pig,
+        file: require('../Images/Species/Pig.png')
+    },
+    {
+        species: SPECIES.rabbit,
+        file: require('../Images/Species/Rabbit.png')
+    },
+    {
+        species: SPECIES.rhino,
+        file: require('../Images/Species/Rhino.png')
+    },
+    {
+        species: SPECIES.sheep,
+        file: require('../Images/Species/Sheep.png')
+    },
+    {
+        species: SPECIES.squirrel,
+        file: require('../Images/Species/Squirrel.png')
+    },
+    {
+        species: SPECIES.tiger,
+        file: require('../Images/Species/Tiger.png')
+    },
+    {
+        species: SPECIES.wolf,
+        file: require('../Images/Species/Wolf.png')
+    }
+];
+
+export { GENDER, SPECIES, SPECIES_IMG };
